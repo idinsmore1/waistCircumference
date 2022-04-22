9c6aae2e6e8caf7b9c3bc4bd331474f7c2ba7b1b
irdinsmore1 <irdinsmore1@geisinger.edu>
2022-04-22 10:34:10 -0400

    added calculations around the center measurement

--- waistCircumference/calculate_waist_circumference.py
+++ waistCircumference/calculate_waist_circumference.py
@@ -3,29 +3,59 @@ import argparse
 import pandas as pd
 
 from dicomseries import DicomSeries
-from waistCircumference.utilities import binarize_scan, fill_binary_gaps, mark_body, measure_circumference
+from utilities import (
+    binarize_scan,
+    fill_binary_gaps,
+    mark_body,
+    measure_circumference,
+    detect_number_of_bones,
+    get_waist_range,
+    select_waist_measurement
+)
 
 # This is a sample Python script.
 parser = argparse.ArgumentParser(description='Automatically Calculate Waist Circumference from CT Scan')
 parser.add_argument('-i', '--input', help='Input DICOM Series Directory', required=True)
 parser.add_argument('-o', '--output', help='Output CSV File Directory', required=True)
+parser.add_argument('-t', '--threshold', help='Bone Area Threshold to filter number of bones', required=False,
+                    default=100)
 args = parser.parse_args()
 
 
 def main():
     dicom_series = DicomSeries(args.input)
-    description = f'MRN{dicom_series.mrn}_ACC{dicom_series.acc}_{dicom_series.cut}_waist_circumference'
+    description = f'MRN{dicom_series.mrn}_ACC{dicom_series.acc}_{dicom_series.cut}_waist_circumferences'
     outfile = f'{args.output}/{description}.csv'
-    ct_scan = dicom_series.read_dicom_series('*', -10, 300)
-    waist_circumferences = []
+    ct_scan = dicom_series.read_dicom_series('*', 0, 500)
+    waist_circumferences = {}
     for image in range(ct_scan.shape[2]):
-        binary_im = binarize_scan(ct_scan[:, :, image])
+        image_slc = ct_scan[:, :, image]
+        binary_im = binarize_scan(image_slc)
         binary_im = fill_binary_gaps(binary_im)
         body_array = mark_body(binary_im)
         measurement = measure_circumference(body_array, dicom_series.series_info['width'])
-        waist_circumferences.append(measurement)
-    df = pd.DataFrame(waist_circumferences, columns=['waist_circumference'])
+        n_bones = detect_number_of_bones(image_slc, upper_bound=250, area_threshold=int(args.threshold))
+        waist_circumferences[image] = {'waist_circumference_cm': measurement, 'n_bones': n_bones}
+    df = pd.DataFrame(waist_circumferences).T
     df.to_csv(outfile)
+    max_ix, waist_range = get_waist_range(df)
+    print(max_ix, waist_range)
+    waist_center, waist_ix, = select_waist_measurement(df, max_ix, waist_range)
+    five_measure = df.loc[(waist_ix - 2):(waist_ix + 2), 'waist_circumference_cm']
+    fifteen_measure = df.loc[(waist_ix - 8):(waist_ix + 8), 'waist_circumference_cm']
+    important_vals = [
+        dicom_series.mrn,
+        dicom_series.series_info['scan_date'],
+        waist_ix,
+        waist_center,
+        five_measure.mean(),
+        five_measure.std(),
+        fifteen_measure.mean(),
+        fifteen_measure.std()
+    ]
+    pd.DataFrame([important_vals],
+                 columns=['MRN', 'ScanDate', 'WaistIndex', 'WaistCenter', '5-Mean', '5-Std', '15-Mean',
+                          '15-Std']).to_csv(f'{args.output}/{description}_measurement.csv', index=False)
 
 
 if __name__ == '__main__':
--- waistCircumference/utilities.py
+++ waistCircumference/utilities.py
@@ -1,9 +1,9 @@
 import numpy as np
 from numba import njit
-from skimage import measure
+from skimage.measure import label, perimeter, regionprops
 
 
-@njit(parallel=True)
+@njit()
 def binarize_scan(pixel_array: np.ndarray) -> np.ndarray:
     """
     Binarize a CT scan between air and body
@@ -22,10 +22,9 @@ def fill_binary_gaps(binary_im: np.ndarray) -> np.ndarray:
     :param binary_im: output from binarize_scan
     :return: filled binary image as numpy array
     """
-    return np.maximum.accumulate(binary_im, 1) & \
-           np.maximum.accumulate(binary_im[:, ::-1], 1)[:, ::-1] & \
-           np.maximum.accumulate(binary_im[::-1, :], 0)[::-1, :] & \
-           np.maximum.accumulate(binary_im, 0)
+    return np.maximum.accumulate(binary_im, 1) & np.maximum.accumulate(binary_im[:, ::-1], 1)[:,
+                                                 ::-1] & np.maximum.accumulate(binary_im[::-1, :], 0)[::-1,
+                                                         :] & np.maximum.accumulate(binary_im, 0)
 
 
 def mark_body(binary_im: np.ndarray) -> np.ndarray:
@@ -34,7 +33,7 @@ def mark_body(binary_im: np.ndarray) -> np.ndarray:
     :param binary_im: output from fill_binary_gaps
     :return: body only as a binary numpy array
     """
-    body = measure.label(binary_im)
+    body = label(binary_im)
     body[body != 1] = 0
     return body
 
@@ -46,6 +45,72 @@ def measure_circumference(body_array, pixel_width):
     :param pixel_width: the width of a pixel in centimeters
     :return: the circumference of the body in centimeters
     """
-    waist_pixels = measure.perimeter(body_array)
+    waist_pixels = perimeter(body_array)
     waist_cm = np.round(waist_pixels * pixel_width, 2)
     return waist_cm
+
+
+def detect_number_of_bones(pixel_array, upper_bound=175, area_threshold=100):
+    """
+    Detect the number of bones in the image
+    :param pixel_array: the dicom pixel array
+    :param upper_bound: the upper bound of the range of bone HU values
+    :return: the number of bones in the image
+    """
+    bone_image = pixel_array >= upper_bound
+    # Remove scan interference
+    bone_image[400:, :] = 0
+    # Label the number of bones
+    labels = label(bone_image)
+    regions = regionprops(labels)
+    # remove small regions that are not important
+    bones = [prop.label for prop in regions if prop.area > area_threshold]
+    return len(bones)
+
+
+def get_waist_range(waist_df):
+    """
+    Get the index of the waist in the dataframe
+    :param waist_df: the dataframe with the waist measurements
+    :return: the index of the waist in the dataframe
+    """
+    incr_ranges = {}
+    for row in waist_df.itertuples():
+        if row.n_bones != 1:
+            continue
+        next_val = row.Index + 1
+        last_val = row.Index
+        max_val = row.n_bones
+        while waist_df.loc[next_val, 'n_bones'] >= waist_df.loc[last_val, 'n_bones']:
+            if next_val + 1 > waist_df.index[-1]:
+                break
+            else:
+                max_val = waist_df.loc[next_val, 'n_bones']
+                last_val = next_val
+                next_val += 1
+        if max_val < 3:
+            continue
+        else:
+            incr_ranges[row.Index] = len([i for i in range(row.Index, next_val)])
+
+    if len(incr_ranges) == 0:
+        print('No waist found')
+        return None
+    else:
+        max_ix = max(incr_ranges, key=incr_ranges.get)
+        waist_range = incr_ranges[max_ix]
+        return max_ix, waist_range
+
+
+def select_waist_measurement(waist_df, max_ix, waist_range):
+    """
+    Select the waist measurement from the dataframe
+    :param waist_df: the dataframe with the waist measurements
+    :param max_ix: the index of the waist in the dataframe
+    :param waist_range: the range of the waist measurements
+    :return: the waist measurement
+    """
+    waist = waist_df.iloc[max_ix:max_ix + waist_range, :]
+    waist_ix = waist[waist['n_bones'] == 3].index.max()
+    waist_center = waist_df.loc[waist_ix, 'waist_circumference_cm']
+    return waist_center, waist_ix
