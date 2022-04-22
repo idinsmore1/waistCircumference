d96d6dc613dbb9db960dbbed66507b3f39b24d8a
irdinsmore1 <irdinsmore1@geisinger.edu>
2022-04-22 10:38:58 -0400

    added some code comments

--- waistCircumference/calculate_waist_circumference.py
+++ waistCircumference/calculate_waist_circumference.py
@@ -1,7 +1,7 @@
 import argparse
 
 import pandas as pd
-
+import os
 from dicomseries import DicomSeries
 from utilities import (
     binarize_scan,
@@ -23,11 +23,16 @@ args = parser.parse_args()
 
 
 def main():
+    # Create a DicomSeries object
     dicom_series = DicomSeries(args.input)
+    if not os.path.exists(args.output):
+        os.makedirs(args.output)
+    # Make the base name of the output file
     description = f'MRN{dicom_series.mrn}_ACC{dicom_series.acc}_{dicom_series.cut}_waist_circumferences'
-    outfile = f'{args.output}/{description}.csv'
+    # Read the DicomSeries object at an HU where bone is easily visible
     ct_scan = dicom_series.read_dicom_series('*', 0, 500)
     waist_circumferences = {}
+    # For each cut, binarize the image, fill the gaps, measure the circumference, and get number of bones
     for image in range(ct_scan.shape[2]):
         image_slc = ct_scan[:, :, image]
         binary_im = binarize_scan(image_slc)
@@ -36,11 +41,14 @@ def main():
         measurement = measure_circumference(body_array, dicom_series.series_info['width'])
         n_bones = detect_number_of_bones(image_slc, upper_bound=250, area_threshold=int(args.threshold))
         waist_circumferences[image] = {'waist_circumference_cm': measurement, 'n_bones': n_bones}
+    # Write the results to a CSV file
     df = pd.DataFrame(waist_circumferences).T
-    df.to_csv(outfile)
+    df.to_csv(f'{args.output}/{description}.csv')
+    # Select the waist measurement from the CSV file
     max_ix, waist_range = get_waist_range(df)
     print(max_ix, waist_range)
     waist_center, waist_ix, = select_waist_measurement(df, max_ix, waist_range)
+    # Store the data around the waist to calculate the mean and standard deviation
     five_measure = df.loc[(waist_ix - 2):(waist_ix + 2), 'waist_circumference_cm']
     fifteen_measure = df.loc[(waist_ix - 8):(waist_ix + 8), 'waist_circumference_cm']
     important_vals = [
@@ -53,6 +61,7 @@ def main():
         fifteen_measure.mean(),
         fifteen_measure.std()
     ]
+    # Write the important values to a CSV file
     pd.DataFrame([important_vals],
                  columns=['MRN', 'ScanDate', 'WaistIndex', 'WaistCenter', '5-Mean', '5-Std', '15-Mean',
                           '15-Std']).to_csv(f'{args.output}/{description}_measurement.csv', index=False)
